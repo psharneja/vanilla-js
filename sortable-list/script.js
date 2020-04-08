408f7713175fb6ce2ee3085fc6dc69422d600cf1
Prabhsimran Singh (Simar Harneja) <psharneja@gmail.com>
2020-04-09 02:39:36 +0530

    dragndrop

--- sortable-list/script.js
+++ sortable-list/script.js
@@ -1,15 +1,99 @@
-const draggable_list = document.getElementById('draggable-list');
-const check = document.getElementById('check');
+const draggable_list = document.getElementById("draggable-list");
+const check = document.getElementById("check");
 
 const richestPeople = [
-  'Jeff Bezos',
-  'Bill Gates',
-  'Warren Buffett',
-  'Bernard Arnault',
-  'Carlos Slim Helu',
-  'Amancio Ortega',
-  'Larry Ellison',
-  'Mark Zuckerberg',
-  'Michael Bloomberg',
-  'Larry Page'
+  "Jeff Bezos",
+  "Bill Gates",
+  "Warren Buffett",
+  "Bernard Arnault",
+  "Carlos Slim Helu",
+  "Amancio Ortega",
+  "Larry Ellison",
+  "Mark Zuckerberg",
+  "Michael Bloomberg",
+  "Larry Page",
 ];
+function swapItems(fromIndex, toIndex) {
+  const itemOne = listItems[fromIndex].querySelector(".draggable");
+  const itemTwo = listItems[toIndex].querySelector(".draggable");
+  listItems[fromIndex].appendChild(itemTwo);
+  listItems[toIndex].appendChild(itemOne);
+}
+
+const listItems = [];
+
+let dragStartIndex;
+function dragStart() {
+  dragStartIndex = +this.closest("li").getAttribute("data-index");
+}
+function dragDrop() {
+  const dragEndIndex = +this.getAttribute("data-index");
+  swapItems(dragStartIndex, dragEndIndex);
+  this.classList.remove("over");
+}
+function dragOver(e) {
+  e.preventDefault();
+}
+function dragEnter() {
+  this.classList.add("over");
+}
+function dragLeave() {
+  this.classList.remove("over");
+}
+
+function checkOrder() {
+  listItems.forEach((listItem, index) => {
+    const personName = listItem.querySelector(".draggable").innerText.trim();
+
+    if (personName !== richestPeople[index]) {
+      listItem.classList.remove("right");
+
+      listItem.classList.add("wrong");
+    } else {
+      listItem.classList.remove("wrong");
+      listItem.classList.add("right");
+    }
+  });
+}
+
+function addEventListeners() {
+  const draggables = document.querySelectorAll(".draggable");
+  const dragListItems = document.querySelectorAll(".draggable-list li");
+
+  draggables.forEach((draggable) => {
+    draggable.addEventListener("dragstart", dragStart);
+  });
+
+  dragListItems.forEach((item) => {
+    item.addEventListener("dragover", dragOver);
+    item.addEventListener("drop", dragDrop);
+    item.addEventListener("dragenter", dragEnter);
+    item.addEventListener("dragleave", dragLeave);
+  });
+}
+
+function createList() {
+  [...richestPeople]
+    .map((a) => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((a) => a.value)
+    .forEach((person, index) => {
+      const listItem = document.createElement("li");
+      listItem.setAttribute("data-index", index);
+      listItem.innerHTML = `
+    <span class="number">${index + 1}</span>
+    <div class="draggable" draggable="true">
+    <p class="person-name">${person}</p>
+    <i class="fas fa-grip-lines"></i>
+    </div>
+    `;
+      listItems.push(listItem);
+      draggable_list.appendChild(listItem);
+    });
+
+  addEventListeners();
+}
+
+createList();
+
+check.addEventListener("click", checkOrder);
